0baad88f62316bc0121e10a39c659165d83b25b7
Ahmed3Elkallaf <ahmedelkallaf77@gmail.com>
2024-07-12 14:03:05 +0300

    Add files via upload

---
+++ g.js
@@ -0,0 +1,52 @@
+var score = 0
+
+btn.onclick = function click() {
+
+score++
+
+document.getElementById("score").innerText = score
+
+if (score >= "20"){
+
+score++
+
+
+}
+
+if (score >= "70"){
+
+score++
+
+
+}
+
+if (score >= "170"){
+
+
+score++
+
+}
+
+if (score>= "320"){
+
+    score++
+
+
+
+}
+
+
+if (score>= "520"){
+
+    score++
+
+}
+
+if (score>= "770"){
+
+
+score++
+}
+    
+
+}
